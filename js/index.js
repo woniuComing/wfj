5b0fa872e1fc379e9086bb39929c35bc4232337c
woniu <you@example.com>
2016-09-05 01:14:32 +0800

    wfj

---
+++ js/index.js
@@ -0,0 +1,10 @@
+
+window.onload=function(){
+        var mySwiper = new Swiper ('.swiper-container', {
+            // direction: 'vertical',
+            loop: true,
+            autoplay:1000,
+            autoplayDisableOnInteraction : false,
+            pagination: '.swiper-pagination',
+        })
+    }
